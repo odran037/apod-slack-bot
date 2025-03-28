7a96b4e37bcdd4d4ebec619e4438b047e09ad831
Leonardo Prates <odran037@users.noreply.github.com>
2025-03-28 03:47:15 +0000

    blocks

--- post_apod.js
+++ post_apod.js
@@ -9,25 +9,34 @@ async function postToSlack() {
   try {
     const response = await axios.get(NASA_API_URL);
     const { title, url, explanation } = response.data;
-    // const message = `🌌 *NASA Astronomy Picture of the Day* 🌌\n*${title}*\n${url}\n_${explanation}_`;    
-    // const slack = new WebClient(SLACK_BOT_TOKEN);
+    const message = `🌌 *NASA Astronomy Picture of the Day* 🌌\n*${title}*\n${url}\n_${explanation}_`;
+    const slack = new WebClient(SLACK_BOT_TOKEN);
 
     // await slack.chat.postMessage({
     //   channel: SLACK_CHANNEL,
     //   text: message,
     //   unfurl_links: true,
-    //   unfurl_media: true
+    //   unfurl_media: true,
     // });
-    const message = `🌌 *NASA Astronomy Picture of the Day* 🌌\n*${title}*\n${url}\n_${explanation}_`;
-    const slack = new WebClient(SLACK_BOT_TOKEN);
 
     await slack.chat.postMessage({
       channel: SLACK_CHANNEL,
-      text: message,
-      unfurl_links: true,
-      unfurl_media: true,
+      text: `🌌 *NASA Astronomy Picture of the Day* 🌌\n*${title}*`,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `🌌 *NASA Astronomy Picture of the Day* 🌌\n*${title}*\n_${explanation}_`,
+          },
+        },
+        {
+          type: "image",
+          image_url: url,
+          alt_text: title,
+        },
+      ],
     });
-
 
     console.log("Posted to Slack successfully!");
   } catch (error) {
